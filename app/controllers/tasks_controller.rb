9100aff8b3371e46930cd417dac614ee5e62f968
Faris Chew <farischew@gmail.com>
2023-03-07 13:07:39 +0800

    Created edit and update, destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,7 +19,21 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to task_path, status: :see_other
   end
 
   private
